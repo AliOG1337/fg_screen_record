516229c4525520071b8a42ed6c124a32548fc721
AliOG <135515727+AliOG1337@users.noreply.github.com>
2023-12-06 20:02:55 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,9 +3,5 @@ Config = {}
 Config.Anticheatname = exports["NAME"] --CHANGE "NAME" TO YOUR RESSOURCENAME OF FIVEGUARD
 
 Config.AllowedGroups = {'admin', 'mod'} -- Add here Groups 
-
 Config.RecordCommand = "record"
-Config.RecordCommandAce = "test"
-
 Config.ScreenCommand = "screenshot"
-Config.ScreenCommandAce = "test"
